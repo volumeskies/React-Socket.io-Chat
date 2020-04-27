5c82f4bc330647b3c15f300fa2f7c1c1f8f0602e
Tatyana Krivorotova <krivorotovatd@gmail.com>
2020-04-27 16:36:31 +0300

    ChatRoom component created

---
+++ src/components/ChatRoom/ChatRoom.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import './ChatRoom.scss';
+
+class ChatRoom extends React.Component{
+    render(){
+        return(
+            <h2>world!!!</h2>
+        );
+    }
+}
+export default ChatRoom;
