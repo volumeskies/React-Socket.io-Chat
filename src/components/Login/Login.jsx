706ae5b9e24f70a7d57c742ecfcffda079b10a1f
Tatyana Krivorotova <krivorotovatd@gmail.com>
2020-04-27 16:35:27 +0300

    Modified Login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import './App.scss';
+import './Login.scss';
 
 class Login extends React.Component{
 render(){
     return(
-        <h1>Hello!</h1>
+        <header class='header'>
+            <h1 class='header__heading'>welcome to chat</h1>
+            <input class='header__input' type='text' maxLength='20' title=' ' required/>
+            <label class='header__label'>enter your name</label>
+        </header>
     );
 }
 };
