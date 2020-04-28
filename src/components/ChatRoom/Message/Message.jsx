ef74e7f1485547f54ba7c5e284cc8ed18ab464d8
Tatyana Krivorotova <krivorotovatd@gmail.com>
2020-04-28 20:17:46 +0300

    New Message component

---
+++ src/components/ChatRoom/Message/Message.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Message.scss';
+
+class Message extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return(
+            <h1>ssds</h1>
+        );
+    }
+}
+
+export default Message;
