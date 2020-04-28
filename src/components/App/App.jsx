f000c23854be88f8d374023e0ab6e546e8150b32
Tatyana Krivorotova <krivorotovatd@gmail.com>
2020-04-28 20:15:25 +0300

    Improved routing for ChatRoom

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.scss';
 import Login from '../Login/Login.jsx';
 import ChatRoom from '../ChatRoom/ChatRoom.jsx'
+import {BrowserHistory} from 'react-router'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 class App extends React.Component{
+    constructor(props) {
+        super(props);
+    }
     render(){
         return(
-            <Router>
-                <Route path='/' exact component={Login}/>
-                <Route path='/room:id' exact component={ChatRoom}/>
+            <Router history={BrowserHistory}>
+                <Route path='/' exact component={() => (<Login socket={this.props.socket}/>)}/>
+                <Route path='/room/:id' exact component={(props) => (<ChatRoom {...props} socket={this.props.socket}/>)}/>
             </Router>
         );
     }
