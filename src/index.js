b4947c8f5b4116596d2a555499402e1b262074ba
Tatyana Krivorotova <krivorotovatd@gmail.com>
2020-04-28 20:16:56 +0300

    Added socket.io to client

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App.jsx';
+import io from "socket.io-client";
+const URL = "http://localhost:8080";
+const socket = io(URL);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App socket={socket}/>, document.getElementById('root'));
