369131bbea859e4e19d37a040deb5d666dda7d1b
Tatyana Krivorotova <krivorotovatd@gmail.com>
2020-04-28 20:16:23 +0300

    Is user logged in check

--- src/components/ChatRoom/ChatRoom.jsx
+++ src/components/ChatRoom/ChatRoom.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './ChatRoom.scss';
+import ListUsers from "././ListUsers/ListUsers.jsx";
+import Chat from "././Chat/Chat.jsx";
 
 class ChatRoom extends React.Component{
+
+    componentDidMount() {
+        this.props.location.state === undefined? console.log('meh'):console.log('yeeeeeeaa');
+    }
+
     render(){
         return(
-            <h2>world!!!</h2>
+            <>
+                <ListUsers/>
+                <Chat/>
+            </>
         );
     }
 }
