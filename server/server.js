e232ed155b8bd153eacf41e59091144007bdeee7
volumeskies <krivorotovatd@gmail.com>
2020-04-30 22:06:48 +0300

    Added events handling list of users

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const server = app.listen(port);
 const io = socket(server);
 
 /* List of all connected users */
-let users = {};
+let users = [];
 
 /* An array of current rooms */
 let rooms = [];
@@ -29,6 +29,7 @@ let rooms = [];
 /* Handling user's behaviour in app */
 io.sockets.on('connection', function(socket) {
     console.log("server connection with socket: ", socket.id);
+    // User logged in event
     socket.on('login', data=>{
         // Temporary user plain object
         let user = {
@@ -36,20 +37,42 @@ io.sockets.on('connection', function(socket) {
             socket: '',
             room: '',
         }
-        // Generates random room id
-        let roomID = shortid.generate();
-        socket.join(`${roomID}`);
+
+        // If user was invited -> initializing with copied room id
+        // else initializing with random room id (creating a new room)
+        let roomID = data.invited ? data.invited.match(/([^/]*)$/)[0] : shortid.generate();
 
         user.username = data.username;
         user.socket = socket.id;
         user.room = roomID;
-        users[socket.id] = user;
+        users.push(user);
 
         rooms.push(roomID);
+
+        socket.join(`${roomID}`);
+
+        // Send to client generated room id
         socket.emit('roomID', {roomID: roomID});
+
+        // Send to everyone except current socket
+        socket.to(`${roomID}`).emit('userJoined', {username: user.username});
 
         console.log(users);
     });
 
+    socket.on('getUsers', data => {
+       let listUsers = [];
+       users.forEach(elem => {
+           console.log('elem', elem.username, data.roomID);
+           if(elem.room === data.roomID) {
+               listUsers.push(elem.username);
+               console.log(elem.username);
+           }
+       });
+
+       console.log('list', listUsers);
+
+       socket.emit('listUsers', {users: listUsers});
+    });
 
 });
