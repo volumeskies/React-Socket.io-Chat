30cd1a644b39da7c950e149c7fa16e6264173ae0
volumeskies <krivorotovatd@gmail.com>
2020-04-30 22:13:02 +0300

    Implemented logic with users' list and messages

--- src/components/ChatRoom/ChatRoom.jsx
+++ src/components/ChatRoom/ChatRoom.jsx
@@ -3,18 +3,37 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './ChatRoom.scss';
 import ListUsers from "././ListUsers/ListUsers.jsx";
 import Chat from "././Chat/Chat.jsx";
+import Redirect from "react-router-dom/es/Redirect";
 
 class ChatRoom extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            logged: this.props.location.state !== undefined,
+            roomID: this.props.location.state !== undefined? this.props.location.state.roomID : '',
+        }
+    }
 
-    componentDidMount() {
-        this.props.location.state === undefined? console.log('meh'):console.log('yeeeeeeaa');
+    componentDidMount(){
+        this.props.socket.emit('getUsers', {roomID: this.state.roomID});
+        this.props.socket.emit('getMessages', {roomID: this.state.roomID});
     }
 
     render(){
         return(
             <>
-                <ListUsers/>
-                <Chat/>
+                {this.state.logged?
+                    <>
+                        <ListUsers socket={this.props.socket}/>
+                        <Chat socket={this.props.socket}/>
+                    </>
+                    :
+                    <Redirect to={{
+                        pathname: `/`,
+                        state: {
+                            roomID: window.location.pathname,
+                        }}} />
+                }
             </>
         );
     }
