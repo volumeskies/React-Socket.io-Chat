75ffc4a01e31872d2ff2d1580edcbe20a16727dd
volumeskies <krivorotovatd@gmail.com>
2020-05-01 20:57:59 +0300

    Fixed Message component's structure

--- src/components/ChatRoom/Message/Message.jsx
+++ src/components/ChatRoom/Message/Message.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import './Message.scss';
 
-class Message extends React.Component{
-    constructor(props) {
-        super(props);
-    }
-
-    render(){
-        return(
-            <h1>ssds</h1>
-        );
-    }
+const Message = (props) => {
+    return(
+        <div className='message'>
+            <span className='message__sender'>{props.username}</span>
+            <span className='message__date'>{props.date}</span>
+            <p className='message__text'>{props.text}</p>
+        </div>
+    );
 }
 
 export default Message;
