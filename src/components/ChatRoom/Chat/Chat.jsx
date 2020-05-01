adf94a4c44668af0b3520f7f6398f48bb2d86aad
volumeskies <krivorotovatd@gmail.com>
2020-05-01 21:00:25 +0300

    Implemented chat logic

--- src/components/ChatRoom/Chat/Chat.jsx
+++ src/components/ChatRoom/Chat/Chat.jsx
@@ -1,18 +1,47 @@
 import React from 'react'
 import './Chat.scss';
+import Message from '.././Message/Message.jsx';
 
 class Chat extends React.Component{
     constructor(props) {
         super(props);
+        this.state={
+            messages: []
+        }
+        this.handleSendClick = this.handleSendClick.bind(this);
+        this.textRef = React.createRef();
+        this.chatEnd = React.createRef();
+    }
+
+    componentDidMount() {
+        this.props.socket.on('chatHistory', data=>{
+            this.setState({messages: data.messages});
+        })
+        this.props.socket.on('newMessage', data=>{
+            this.setState({messages: this.state.messages.concat(data)});
+        })
+    }
+
+    componentDidUpdate(){
+        this.chatEnd.current.scrollIntoView({behavior: 'smooth'});
+    }
+
+    handleSendClick(){
+        this.props.socket.emit('sendMessage', {message: this.textRef.current.value});
+        this.textRef.current.value = '';
     }
 
     render(){
+        const messages = this.state.messages.map((message)=>
+            <Message username={message.sender} date={message.date} text={message.text}/>);
         return(
             <div className='chat'>
-                <div className='chat__window'></div>
-                <textarea className='chat__input' placeholder='Enter your message...'>
-                </textarea>
-                <a className='chat__send'>Send</a>
+                <div className='chat__window'>
+                    {messages}
+                    <div ref={this.chatEnd}></div>
+                </div>
+                <textarea ref={this.textRef} className='chat__input' placeholder='Enter your message...'/>
+                <a className='chat__send' onClick={this.handleSendClick}>Send</a>
             </div>
         );
     }
