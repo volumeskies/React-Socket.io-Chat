28e2081ff95c676f02ab966c2183ee3374e98c93
Tatiana Krivorotova <krivorotovatd@gmail.com>
2023-05-07 18:14:43 +0100

    test commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App/App.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App/App.jsx";
 import io from "socket.io-client";
 const URL = "http://localhost:8080";
 const socket = io(URL);
+const test = "test";
 
-ReactDOM.render(<App socket={socket}/>, document.getElementById('root'));
+ReactDOM.render(<App socket={socket} />, document.getElementById("root"));
